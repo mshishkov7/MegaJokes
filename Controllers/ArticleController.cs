ad75653c2ed3e9a0f59632ce7d6dfd991696e102
Martin Shishkov <verylosh@gmail.com>
2016-12-17 12:54:34 +0200

    cool and working
    yea

---
+++ Controllers/ArticleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+            return 
+                View();
+        }
+    }
+}
---
+++ Models/BlogDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Blog.Models
+{
+    public class BlogDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public BlogDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static BlogDbContext Create()
+        {
+            return new BlogDbContext();
+        }
+    }
+}
