26d98cdeec9cc7fbdb98ba298da83807c8f2a11b
Martin Shishkov <verylosh@gmail.com>
2016-12-17 16:56:18 +0200

    started editing of users!
    started editing of users!

---
+++ Controllers/Admin/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blog.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Blog.Controllers.Admin
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        //
+        //GET: User/List
+        public ActionResult List()
+        {
+            using (var database = new BlogDbContext())
+            {
+                var users = database.Users.ToList();
+
+                var admins = GetAdminUserNames(users, database);
+                ViewBag.Admins = admins;
+
+                return View(users);
+            }
+        }
+
+        private HashSet<string> GetAdminUserNames(List<ApplicationUser> users, BlogDbContext context)
+        {
+            var userManager = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context));
+
+            var admins = new HashSet<string>();
+
+            foreach (var user in users)
+            {
+                if (userManager.IsInRole(user.Id, "Admin"))
+                {
+                    admins.Add(user.UserName);
+                }
+            }
+            return admins;
+        }
+
+    }
+}
