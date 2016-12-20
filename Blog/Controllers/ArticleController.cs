554c71512abcaef027fd1f0d27831f3f6d32b327
Martin Shishkov <verylosh@gmail.com>
2016-12-20 14:09:06 +0200

    fixed the starting
    fixed the starting

---
+++ Blog/Controllers/ArticleController.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Blog.Models;
+
+namespace Blog.Controllers
+{
+    public class ArticleController : Controller
+    {
+
+        //
+        // GET: Article
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        //
+        //Get Article/List
+        public ActionResult List()
+        {
+            using (var database = new BlogDbContext())
+            {
+                var articles = database.Articles
+                    .Include(a => a.Author)
+                    .Include(a=>a.Tags)
+                    .ToList();
+
+                return View(articles);
+            }
+        }
+        //
+        // Get Article/Details
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            using (var database = new BlogDbContext())
+            {
+                var article = database.Articles
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .Include(a => a.Tags)
+                    .First();
+
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(article);
+            }
+        }
+        //
+        // GET: Article/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            using (var database = new BlogDbContext())
+            {
+                var model = new ArticleViewModel();
+                model.Categories = database.Categories
+                    .OrderBy(c => c.Name)
+                    .ToList();
+
+                return View(model);
+            }
+        }
+
+        //
+        //POST: Article/Create
+        [HttpPost]
+        [Authorize]
+        public ActionResult Create(ArticleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var database = new BlogDbContext())
+                {
+                    var authorId = database.Users
+                        .Where(u => u.UserName == this.User.Identity.Name)
+                        .First()
+                        .Id;
+
+                    var article = new Article(
+                        authorId, model.Title,
+                        model.Content, model.CategoryId);
+
+                    this.SetArticleTags(article, model, database);
+
+                    // save article in db
+                    database.Articles.Add(article);
+                    database.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(model);
+        }
+
+        //
+        // GET: Article/Delete
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                var article = database.Articles
+                .Where(a => a.Id == id)
+                .Include(a => a.Author)
+                .Include(a => a.Category)
+                .First();
+
+                //Check if article exist
+                if (!IsUserAuthorizedToEdit(article))
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+
+                ViewBag.TagsString = string.Join(", ", article.Tags.Select(t => t.Name));
+
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                // pass article to view
+                return View(article);
+            }
+        }
+        //
+        //POST: Article/Delete
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            using (var database = new BlogDbContext())
+            {
+                //Get Article from database
+                var article = database.Articles
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .First();
+
+                //check if article exist
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                //delete article from database
+                database.Articles.Remove(article);
+                database.SaveChanges();
+
+                //Redirect to index page
+                return RedirectToAction("Details", article);
+
+            }
+        }
+
+        //
+        // GET: Article/Edit
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                //Get Article from database
+                var article = database.Articles
+                    .Where(a => a.Id == id)
+                    .First();
+
+                //Check if article exist
+                if (!IsUserAuthorizedToEdit(article))
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+                // create a view model
+                var model = new ArticleViewModel();
+                model.Id = article.Id;
+                model.Title = article.Title;
+                model.Content = article.Content;
+                model.CategoryId = article.CategoryId;
+                model.Categories = database.Categories
+                    .OrderBy(c => c.Name).ToList();
+
+                model.Tags = string.Join(", ", article.Tags.Select(t => t.Name));
+
+                //pass the view model to view
+                return View(model);
+            }
+        }
+        //
+        // POST: Article/Edit
+        [HttpPost]
+        public ActionResult Edit(ArticleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var database = new BlogDbContext())
+                {
+                    //Get Article from database
+                    var article = database.Articles
+                        .FirstOrDefault(a => a.Id == model.Id);
+                    //set prop
+                    article.Title = model.Title;
+                    article.Content = model.Content;
+                    article.CategoryId = model.CategoryId;
+                    this.SetArticleTags(article, model, database);
+                    //save article
+                    database.Entry(article).State = EntityState.Modified;
+                    database.SaveChanges();
+
+                    //redirect
+                    return RedirectToAction("Details", article);
+                }
+            }
+            return View(model);
+        }
+
+        private void SetArticleTags(Article article, ArticleViewModel model, BlogDbContext database)
+        {
+            //Split tags
+            var tagsStrings = model.Tags
+                .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(t => t.ToLower())
+                .Distinct();
+            //crear all current tags
+            article.Tags.Clear();
+            //set new tags
+            foreach (var tagsString in tagsStrings)
+            {
+                Tag tag = database.Tags.FirstOrDefault(t => t.Name.Equals(tagsString));
+
+                if (tag == null)
+                {
+                    tag = new Tag() { Name = tagsString };
+                    database.Tags.Add(tag);
+                }
+                article.Tags.Add(tag);
+            }
+
+        }
+
+        private bool IsUserAuthorizedToEdit(Article article)
+        {
+            bool isAdmin = this.User.IsInRole("Admin");
+            bool isAuthor = article.IsAuthor(this.User.Identity.Name);
+
+            return isAdmin || isAuthor;
+        }
+
+        //
+        //GET:
+    }
+}
