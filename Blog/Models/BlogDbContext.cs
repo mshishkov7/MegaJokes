2a0ba19feb9f72c41c6aa6342a28db5dc9218c84
Martin Shishkov <verylosh@gmail.com>
2016-12-20 15:01:34 +0200

    all working !
    all working !

---
+++ Blog/Models/BlogDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Blog.Models
+{
+    public class BlogDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public BlogDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public virtual IDbSet<Article> Articles { get; set; }
+
+        public virtual IDbSet<Category> Categories { get; set; }
+
+        public virtual IDbSet<Tag> Tags { get; set; }
+
+        public static BlogDbContext Create()
+        {
+            return new BlogDbContext();
+        }
+    }
+}
